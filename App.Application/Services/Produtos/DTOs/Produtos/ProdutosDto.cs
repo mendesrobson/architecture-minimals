2235f1c5d3c1bcb72d7c70dd7303c8e9e59a9744
Robson Mendes de Oliveira <robsonmendes_1@outlook.com>
2023-01-22 19:39:48 -0300

    ddd

---
+++ App.Application/Services/Produtos/DTOs/Produtos/ProdutosDto.cs
@@ -0,0 +1,20 @@
+﻿namespace Application.Services.UsuarioPerfil.DTOs.Usuario;
+
+public class ProdutosDto
+{
+    public string Nome { get; private set; }
+    public decimal Preco { get; private set; }
+    public List<string> Tags { get; private set; }
+
+    public static ProdutosDto FromUsuario(string nome,
+                                          decimal Preco, 
+                                          List<string> tags)
+    {
+        return new ProdutosDto
+        {
+            Nome = nome,
+            Preco = Preco,
+            Tags = tags
+        };
+    }
+}
---
+++ App.Domain/Aggregates/Produtos/ProdutosAggregate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain.Aggregates.Produtos
+{
+    internal class ProdutosAggregate
+    {
+        private ProdutosAggregate() { }
+
+        public Guid IdProduto { get; private set; }
+        public string Nome { get; private set; }
+
+        public decimal Preco { get; private set; }
+
+        public List<string> Tags { get; private set; }
+        public DateTime DataCriacao { get; private set; }
+        public DateTime DataModificacao { get; private set; }
+
+        public static ProdutosAggregate CreateProdutos(string nome,
+                                                            decimal preco,
+                                                            List<string> tags)
+        {
+            return new ProdutosAggregate
+            {
+                IdProduto = Guid.NewGuid(),
+                Nome = nome,
+                Preco = preco,
+                Tags = tags,
+                DataCriacao = DateTime.UtcNow,
+                DataModificacao = DateTime.UtcNow
+            };
+        }
+
+        //public void UpdateProdutos(InfoUsuarios usuario)
+        //{
+        //    Usuario = usuario;
+        //    DataModificacao = DateTime.UtcNow;
+        //}
+    }
+}
+}
